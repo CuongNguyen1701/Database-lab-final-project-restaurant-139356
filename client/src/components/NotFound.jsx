79710276a1ec92eba7c09ca5ceabfbeadd73a48e
CuongNguyen1701 <62497480+CuongNguyen1701@users.noreply.github.com>
2023-07-10 01:37:31 +0700

    Create NotFound.jsx

---
+++ client/src/components/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="select-none bg-[#232831] flex flex-col items-center p-32 gap-32">
+      <h1 className="text-6xl font-extrabold">Not Found</h1>
+
+      <img src="/about-img.png" className="h-96"></img>
+    </div>
+  );
+};
+
+export default NotFound;
