a1ead1eca3e6654f30c5995c0bd8c08fea2c4122
CuongNguyen1701 <62497480+CuongNguyen1701@users.noreply.github.com>
2023-07-10 23:59:13 +0700

    generallize input field, add image input

--- client/src/components/InputFields/InputField.jsx
+++ client/src/components/InputFields/InputField.jsx
@@ -1,24 +1,57 @@
 import React from "react";
 
 const styles =
-  "border-2 border-gray-300 rounded-md py-3 px-5 w-full text-xl text-white";
-const SelectField = ({ value, setValue, placeholder }) => {
+  "border-2 border-gray-300 rounded-md py-3 px-5 w-full text-xl text-white bg-primary-light focus:outline-none focus:ring-2 focus:border-transparent";
+
+const ImageUploadField = ({
+  title = "Upload Image",
+  value,
+  setValue,
+  placeholder,
+}) => {
+  return !value ? (
+    <div className="flex flex-col items-center ">
+      <label
+        for="file-upload"
+        className={`${styles} bg-primary-light cursor-pointer hover:bg-primary-light/75`}
+      >
+        {title}
+        <input
+          id="file-upload"
+          type="file"
+          onChange={(e) => setValue(e.target.value)}
+          className={`self-center hidden`}
+        />
+      </label>
+    </div>
+  ) : (
+    <div></div>
+  );
+  S;
+};
+const SelectField = ({ value, setValue, placeholder, options }) => {
   return (
     <select
       value={value}
-      placeholder="how many persons?"
+      placeholder={placeholder}
       className={styles}
       onChange={(e) => setValue(e.target.value)}
     >
-      <option value="1">1 Person</option>
-      <option value="2">2 Persons</option>
-      <option value="3">3 Persons</option>
-      <option value="4">4 Persons</option>
-      <option value="5">5 Persons</option>
+      {options.map((option, index) => (
+        <option key={index} value={option.value}>
+          {option.text}
+        </option>
+      ))}
     </select>
   );
 };
-const InputField = ({ value, setValue, placeholder, type }) => {
+const InputField = ({
+  value,
+  setValue,
+  placeholder,
+  type,
+  bgWhite = false,
+}) => {
   return (
     <input
       type={type}
@@ -30,4 +63,4 @@ const InputField = ({ value, setValue, placeholder, type }) => {
   );
 };
 
-export { SelectField, InputField };
+export { SelectField, InputField, ImageUploadField };
