ea6ceda51c18dabf00ce2dd5b4725a51147657ad
CuongNguyen1701 <62497480+CuongNguyen1701@users.noreply.github.com>
2023-07-10 23:57:54 +0700

    replace div with external component

--- client/src/components/Buttons/LoginButton.jsx
+++ client/src/components/Buttons/LoginButton.jsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
-import GoogleButton from "./GoogleButton";
-import FacebookButton from "./FacebookButton";
-import { Link } from "react-router-dom";
+import Signin from "../Account/Signin";
 const LoginButton = () => {
   const [overlay, setOverlay] = useState(false);
   return (
@@ -15,29 +13,7 @@ const LoginButton = () => {
       >
         Login
       </button>
-      {overlay && (
-        <div
-          className="fixed left-0 right-0 top-0 bottom-0 bg-slate-500/75 flex items-center justify-center z-50"
-          onClick={(e) => {
-            e.preventDefault();
-            setOverlay(false);
-          }}
-        >
-          <div className="p-5 flex flex-col items-center gap-5 bg-white rounded-2xl z-100">
-            <div className="text-primary text-3xl">Log-in</div>
-            <GoogleButton />
-            <FacebookButton />
-            <div className="text-primary">
-              Don't have an account?{" "}
-              <Link to="/signup" className="text-blue-600">
-                {" "}
-                Sign up
-              </Link>{" "}
-              now!
-            </div>
-          </div>
-        </div>
-      )}
+      {overlay && <Signin setOverlay={setOverlay} />}
     </div>
   );
 };
